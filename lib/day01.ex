594ffa160353d4292b70e76b2ec550f27af5a58c
Brett Crowell <brettcrowell@gmail.com>
2017-12-02 22:54:40 -0500

    basic mix project starting point

---
+++ lib/day01.ex
@@ -0,0 +1,5 @@
+defmodule Day01 do
+  def solve(captcha) do
+    captcha
+  end
+end
---
+++ test/day01_test.exs
@@ -0,0 +1,6 @@
+defmodule Day01Test do
+  use ExUnit.Case
+  test "returns input" do
+    assert Day01.solve(123) == 123
+  end
+end
